cbd3803d30d5f0040b910ebe43795892814187e4
raluka <badoi.raluca@gmail.com>
2015-02-16 17:35:40 +0100

    Homework 003 - done

--- homework-003/homework-003_spec.rb
+++ homework-003/homework-003_spec.rb
@@ -5,4 +5,8 @@ describe "#make_list" do
   it "should should takes an array of strings and return an array of strings in a numbered list" do
    expect(make_list(["ich", "ni", "san"])).to eq(["1. ich", "2. ni", "3. san"])
   end
+
+  #TODO
+  maybe sanity check
+
 end
